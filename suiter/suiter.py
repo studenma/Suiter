bf931dd8788ff70e792389b24d8a5b91c7504bf2
Martin Studeny <xstude23@stud.fit.vutbr.cz>
2021-01-09 22:25:01 +0100

    Suiter app init

---
+++ suiter/suiter.py
@@ -0,0 +1,12 @@
+"""
+Module provides a generator of test suits
+"""
+
+import sys
+import os
+
+def test_suite_generator():
+	"""
+	Returns a test suite for a given API endpoint with specified characteristics
+	"""
+	print("DEBUG: test_suite_generator()", flush=True)
